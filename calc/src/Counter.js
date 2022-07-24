05f2ca1ba3ccb01ade63a9fd78f2f1acc16ebe65
palich12 <i@agibalov.ru>
2022-07-24 18:46:46 +0300

    init comit

---
+++ calc/src/Counter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import counter from './store/counter'
+import { observer } from 'mobx-react-lite'
+
+const Counter = observer( () => {
+    return (
+    <div className="Counter">
+        {counter.count}
+        <div className="btns">
+            <button className="btn" onClick={()=> counter.increment()}>+</button>
+            <button className="btn" onClick={()=> counter.decrement()}>-</button>
+        </div>
+    </div>
+    );
+})
+
+export default Counter
